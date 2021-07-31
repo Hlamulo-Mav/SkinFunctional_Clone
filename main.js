0b19928391bac4d50e8180ede01666034b3f3883
nothembah <74840172+nothembah@users.noreply.github.com>
2021-07-31 23:17:39 +0200

    updates

--- main.js
+++ main.js
@@ -1,5 +1,84 @@
-import Heading from './dom.js';
+let container = document.querySelector('#navigation')
 
-const navigation = document.querySelector('header');
+let div = document.createElement('div');
+div.style.marginTop = "-10px"
+div.style.display = "flex";
+div.style.justifyContent = "space-between";
 
-navigation.appendChild(Heading.navbar);
+  let contacts = document.createElement('ul');
+    let phone_num = document.createElement('li');
+    let whatsapp_num = document.createElement('li');
+    let email = document.createElement('li');
+
+    let phone_icon = document.createElement('a');
+    let whatsapp_icon = document.createElement('a');
+    let email_icon = document.createElement('a');
+
+    phone_icon.href = "#";
+    whatsapp_icon.href = "#";
+    email_icon.href = "#";
+
+    phone_icon.classList.add("fa", "fa-phone");
+    whatsapp_icon.classList.add("fa", "fa-whatsapp");
+    email_icon.classList.add("fa", "fa-envelope");
+
+    phone_icon.textContent = "  011 234 5678";
+    whatsapp_icon.textContent = "  071 234 5678";
+    email_icon.textContent = "  info@skinfunctional.com";
+
+    phone_num.appendChild(phone_icon);
+    whatsapp_num.appendChild(whatsapp_icon);
+    email.appendChild(email_icon);
+
+    phone_num.style.cssText = "inline-block"
+    whatsapp_num.style.display = "inline-block"
+    email.style.display = "inline-block"
+
+    contacts.appendChild(phone_num);
+    contacts.appendChild(whatsapp_num);
+    contacts.appendChild(email);
+    contacts.style.marginLeft = "-40px"
+
+    let socials = document.createElement('ul');
+    let facebook = document.createElement('li');
+    let twitter = document.createElement('li');
+    let instagram = document.createElement('li');
+    let youtube = document.createElement('li');
+
+    let facebook_icon = document.createElement('a');
+    let twitter_icon = document.createElement('a');
+    let instagram_icon = document.createElement('a');
+    let youtube_icon = document.createElement('a');
+
+    facebook_icon.href = "#";
+    twitter_icon.href = "#";
+    instagram_icon.href = "#";
+    youtube_icon.href = "#";
+
+    facebook_icon.classList.add("fa", "fa-facebook");
+    twitter_icon.classList.add("fa", "fa-twitter");
+    instagram_icon.classList.add("fa", "fa-instagram");
+    youtube_icon.classList.add("fa", "fa-youtube");
+
+    facebook.appendChild(facebook_icon);
+    twitter.appendChild(twitter_icon);
+    instagram.appendChild(instagram_icon);
+    youtube.appendChild(youtube_icon);
+
+    facebook.style.display = "inline-block"
+    twitter.style.display = "inline-block"
+    instagram.style.display = "inline-block"
+    youtube.style.display = "inline-block"
+
+    socials.appendChild(facebook);
+    socials.appendChild(twitter);
+    socials.appendChild(instagram);
+    socials.appendChild(youtube);
+
+  div.appendChild(contacts);
+  div.appendChild(socials);
+
+  let hr = document.createElement('hr')
+  hr.style.marginTop = "-7px"
+container.appendChild(div);
+container.appendChild(hr)
