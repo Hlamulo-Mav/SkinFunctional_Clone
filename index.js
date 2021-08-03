521672f7307c11f5048bee674bfa5d5a0e83ce0c
nothembah <74840172+nothembah@users.noreply.github.com>
2021-08-03 23:43:19 +0200

    link
    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Header } from "./Header.js";
 import { Contact } from "./contact.js";
-import { Subscription } from "./Subscription.js";
+import { Footer } from "./Footer.js";
 import { form } from "./form.js";
 
 let body = document.querySelector("body");
@@ -11,4 +11,4 @@ let container = document.querySelector("#container");
 container.appendChild(Header.navbar);
 container.appendChild(Contact.about)
 container.appendChild(form.wrap)
-container.appendChild(Subscription.subscribe);
+container.appendChild(Footer.foot);
