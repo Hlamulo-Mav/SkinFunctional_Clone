f1d7052dad3a1ac96a5bb34248c0ed90d5b7020e
nothembah <74840172+nothembah@users.noreply.github.com>
2021-08-04 01:09:43 +0200

    deletion

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { Header } from "./Header.js";
-import { Map } from "./Map.js";
 import { Contact } from "./contact.js";
 import { Footer } from "./Footer.js";
 import { form } from "./form.js";
@@ -11,6 +10,5 @@ body.style.margin = "0";
 let container = document.querySelector("#container");
 container.appendChild(Header.navbar);
 container.appendChild(Contact.about)
-container.appendChild(Map.location);
 container.appendChild(form.wrap)
 container.appendChild(Footer.foot);
