fc5325d5ff30c143c7366d985f625d5bf809a6dd
nothembah <74840172+nothembah@users.noreply.github.com>
2021-08-03 23:58:30 +0200

    map

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Header } from "./Header.js";
+import { Map } from "./Map.js";
 import { Contact } from "./contact.js";
 import { Footer } from "./Footer.js";
 import { form } from "./form.js";
@@ -10,5 +11,6 @@ body.style.margin = "0";
 let container = document.querySelector("#container");
 container.appendChild(Header.navbar);
 container.appendChild(Contact.about)
+container.appendChild(Map.location);
 container.appendChild(form.wrap)
 container.appendChild(Footer.foot);
