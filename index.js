c801f5c6f2a1c06a9ed3734f8a5416c0a6f7cd78
Hlamulo-Mav <zhlamulo@gmail.com>
2021-08-04 02:29:32 +0200

    Updated Commit

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import { Header } from "./Header.js";
 import { Contact } from "./contact.js";
-<<<<<<< HEAD
-=======
-import { Footer } from "./Footer.js";
->>>>>>> 109ba5c395c2135842f2f7358d2a6cd9cf844ec2
 import { form } from "./form.js";
 import { Footerr } from "./contact_footer.js";
+
 
 let body = document.querySelector("body");
 body.style.padding = "0";
@@ -15,9 +12,4 @@ let container = document.querySelector("#container");
 container.appendChild(Header.navbar);
 container.appendChild(Contact.about)
 container.appendChild(form.wrap)
-<<<<<<< HEAD
 container.appendChild(Footerr.foot)
-
-=======
-container.appendChild(Footer.foot);
->>>>>>> 109ba5c395c2135842f2f7358d2a6cd9cf844ec2
